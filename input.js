7415d696bbaa4c2f7f40c4467bb18bd7269f5e57
jad <jad_dis@hotmail.com>
2019-09-25 19:43:10 +0000

    added banter to input

--- input.js
+++ input.js
@@ -2,32 +2,41 @@ let connection;
 const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;
-  const handleUserInput = (stdin, connection) => {
+  const handleUserInput = (connection) => {
     stdin.on('data', (key) => {
       if (key === '\u0003') {
         process.exit();
       }
       if (key === '\u0077'){
         connection.write("Move: up");
-        console.log("up");
+       // console.log("up");
       }
       if (key === '\u0061'){
         connection.write("Move: left");
-        console.log("left");
+       // console.log("left");
       }
       if (key === '\u0073'){
         connection.write("Move: down");
-        console.log("down");
+       // console.log("down");
       }
       if(key === "\u0064"){
         connection.write("Move: right");
-        console.log("right");
+       // console.log("right");
+      }
+      if (key === "z"){
+        connection.write("Say: can't catch me!");
+      }
+      if (key === "x"){
+        connection.write("Say: you're too slow!");
+      }
+      if (key === "c"){
+        connection.write("Say: too easy");
       }
     });
   }
   stdin.setRawMode(true);
   stdin.setEncoding("utf-8");
-  handleUserInput(stdin, connection);
+  handleUserInput(connection);
   stdin.resume();
   return stdin;
 };

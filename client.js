a39467ee724ba6b1303e96f1c98a7b108f565aa5
jad <jad_dis@hotmail.com>
2019-09-25 17:21:28 +0000

    Updated play and client

--- client.js
+++ client.js
@@ -1,16 +1,21 @@
 const net = require('net');
 
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '172.46.2.204',
     port: 50541
   });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
 
+  conn.on("connect", () => {
+    if(net.Socket){console.log("Successfully connected to the game server");}
+    conn.write("Name: Jad");
+  });
   return conn;
 }
 

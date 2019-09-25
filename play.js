3f9532ce40c098dd04ae84c64c3cbaa05a051464
jad <jad_dis@hotmail.com>
2019-09-25 16:55:05 +0000

    Added client and updated play

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.2.204',
-    port: 50541
-  });
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
-
-console.log('Connecting ...');
+const connect = require("./client");
+console.log("Connecting...");
 connect();
